b4c85f35ad27c497251fa98a222ffe3521517b93
Javier <carriloruiz@gmail.com>
2025-10-06 22:17:10 +0200

    Ejercicios con JavaScript

--- Tema-01/JavaScripts/Ejercicios/Ejercicio2/script.js
+++ Tema-01/JavaScripts/Ejercicios/Ejercicio2/script.js
@@ -1,23 +1,24 @@
     // Tenemos una funcion que crea y añade una tabla de 2 filas y 1 columna al contenedor
     function dibujarTabla() {
     const cont = document.getElementById('container');
-
     // Si ya hay una tabla, la reemplazamos
-    cont.innerHTML = '';
 
+        cont.innerHTML = '';
     const tabla = document.createElement('table');
 
     for (let i = 0; i < 2; i++) {
-    const fila = document.createElement('tr');
-    const celda = document.createElement('td');
-    celda.textContent = "Fila " + (i + 1);
-    fila.appendChild(celda);
-    tabla.appendChild(fila);
-}
+        const fila = document.createElement('tr');
+        const celda = document.createElement('td');
+        celda.textContent = "Fila " + (i + 1);
+        fila.appendChild(celda);
+        tabla.appendChild(fila);
+    }
+        let estiloTabla = "color: blue; font-size: 12pt;";
 
-    cont.appendChild(tabla);
-}
+        tabla.style = estiloTabla;
 
+        cont.appendChild(tabla);
+
+    }
     document.getElementById('btnDibujar').addEventListener('click', dibujarTabla);
-
 
---
+++ Tema-01/JavaScripts/Ejercicios/Ejercicio3/script.js
@@ -0,0 +1,14 @@
+/*const boton = document.getElementById('boton');
+const mensaje1 = document.getElementById('mensaje1');
+const mensaje2 = document.getElementById('mensaje2');
+
+function mostrarMensaje(){
+    alert(mensaje1 + mensaje2);
+}
+
+
+boton.addEventListener('click', mostrarMensaje(){
+
+    mensaje1.textContent = 'Esto es el primer cuadro de texto del ejercicio3 de la asignatura optativa';
+    mensaje2.textContent = 'Esto es el primer cuadro de texto del ejercicio3 de la asignatura optativa';
+}*/
