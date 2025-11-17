2dd31a7a7e708538758749055772903feb49ba49
Javier <carriloruiz@gmail.com>
2025-11-17 18:59:10 +0100

    Repaso Examen JavaScript

--- Tema-01_JavaScript/Repaso Examen/Ejercicio3/script.js
+++ Tema-01_JavaScript/Repaso Examen/Ejercicio3/script.js
@@ -1,21 +1,24 @@
 function tirarDado() {
-    const numero = parseFloat(document.getElementById('numero').value) ;
     const img = document.getElementById('img');
+    let numero = document.getElementById('numero');
 
-    if (numero === 1) {
+    let numeros = Math.floor(Math.random() * 6) + 1;
+    numero.innerHTML = `El numero que te ha tocado es: ${numeros}`;
+
+    if (numeros === 1) {
         img.src = './img/cara1.png';
-    }else if (numero === 2) {
+    }else if (numeros === 2) {
         img.src = './img/cara2.png';
-    }else if (numero === 3) {
+    }else if (numeros === 3) {
         img.src = './img/cara3.png';
-    }else if (numero === 4) {
+    }else if (numeros === 4) {
         img.src = './img/cara4.png';
-    }else if (numero === 5) {
+    }else if (numeros === 5) {
         img.src = './img/cara5.png';
-    }else if (numero === 6) {
+    }else if (numeros === 6) {
         img.src = './img/cara6.png';
-        alert('El resultado del dado es 6')
     }
+
 
 
 }
